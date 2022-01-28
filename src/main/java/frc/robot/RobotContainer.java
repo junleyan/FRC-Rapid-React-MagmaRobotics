b9ee97f985e7a4ae896eb47e16eb6f1efc359b41
Junle Yan <yanjunlereal@gmail.com>
2022-01-28 08:49:06 -1000

    Added button control for lift features

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.Lift_On;
+import frc.robot.commands.Lift_Reverse;
+import frc.robot.commands.Lift_Off;
 import frc.robot.commands.Tank_Drive_Command;
+import frc.robot.subsystems.Lift_Subsystem;
 import frc.robot.subsystems.Tank_Drive_Subsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,6 +26,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public static Tank_Drive_Subsystem TankDrive;
   public static Tank_Drive_Command driving;
+  public static Lift_Subsystem Lift;
 
   static Joystick stick0;
 
@@ -28,6 +34,7 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     TankDrive = new Tank_Drive_Subsystem();
+    Lift = new Lift_Subsystem();
     configureButtonBindings();
   }
 
@@ -40,6 +47,15 @@ public class RobotContainer {
   private void configureButtonBindings() {
     stick0 = new Joystick(Constants.CONTROLLER_PORT);
     TankDrive.setDefaultCommand(new Tank_Drive_Command());
+
+    final JoystickButton A_Button = new JoystickButton(stick0, Constants.A_BUTTON_ID);
+    final JoystickButton B_Button = new JoystickButton(stick0, Constants.B_BUTTON_ID);
+
+    A_Button.whileHeld(new Lift_On());
+    A_Button.whenReleased(new Lift_Off());
+    
+    B_Button.whileHeld(new Lift_Reverse());
+    B_Button.whenReleased(new Lift_Off());
   }
 
   public static double getLeftStickY()

24d785c6001e0252df280d29dbad05c8475d4237
Junle Yan <74688804+junleyan@users.noreply.github.com>
2022-02-02 18:47:40 -0800

    Forward command for autonomous

---
+++ src/main/java/frc/robot/commands/Auto_Forward.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class Auto_Forward extends CommandBase {
+    public double time;
+    public long endTime;
+
+    public Auto_Forward(double timeInMillis) {
+        this.time = timeInMillis;
+        addRequirements(RobotContainer.TankDrive);
+    }
+
+    // Called just before this Command runs the first time
+    public void initialize() {
+        long startTime = System.currentTimeMillis();
+        endTime = (long) (startTime + this.time);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    public void execute() {
+        RobotContainer.TankDrive.driveOn();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    public boolean isFinished() {
+        return System.currentTimeMillis() >= endTime;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        RobotContainer.TankDrive.driveOff();
+
+    }
+
+    protected void interrupted() {
+        end();
+    }
+}
